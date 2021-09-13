400cdb196547d604183679e2da8218527d1f27d6
AlexCassa2210 <alexcascante.689@yahoo.com>
2021-09-13 16:18:33 -0600

    Proyecto

---
+++ BienesRaices/Controllers/PropiedadController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BienesRaices.Controllers
+{
+    public class PropiedadController : Controller
+    {
+        // GET: Propiedad
+        public ActionResult Propiedades()
+        {
+
+
+            return View();
+        }
+    }
+}
